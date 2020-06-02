020c720a9b0b6d4eaf017509b36725b65aedb8b4
nicolas <nicolas.eymenier@gmail.com>
2020-06-01 23:02:33 -0300

    ok first charts designed

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem 'uglifier'
 gem 'webpacker'
 gem "chartkick"
 gem "devise"
+gem 'groupdate'
 
 group :development do
   gem 'web-console', '>= 3.3.0'
--- app/controllers/funds_controller.rb
+++ app/controllers/funds_controller.rb
@@ -5,9 +5,27 @@ class FundsController < ApplicationController
     @aum = Aum.find_by(calendar_id: @calendar.id, fund_id: @fund.id)
     @share = Share.find_by(calendar_id: @calendar.id, fund_id: @fund.id)
     @date = @calendar.day.to_formatted_s(:long_ordinal)
+
+
+    range_aum = Aum.where(fund_id: @fund.id)
+    @history_aum = history(@fund, range_aum)
+
+
+    range_share = Share.where(fund_id: @fund.id)
+    @history_share = history(@fund, range_share)
   end
 
   def index
     @funds = Fund.where(area_id: params[:area])
   end
+
+  def history(fund, datas)
+    history = []
+    datas.each do |data|
+      history << [data.calendar.day, data.value]
+    end
+
+    history
+  end
+
 end
