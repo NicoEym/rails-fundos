93af115e6cf22729af19a01ec58917eb6107d1c0
nicolas <nicolas.eymenier@gmail.com>
2020-06-10 22:01:55 -0300

    solving the problem with the date

--- app/controllers/daily_data_controller.rb
+++ app/controllers/daily_data_controller.rb
@@ -6,7 +6,19 @@ class DailyDataController < ApplicationController
     gestor = Gestor.find_by(name: "Ca Indosuez Wealth (Brazil) S.A. Dtvm")
     @funds = Fund.where(gestor: gestor)
 
-    @date = Calendar.last.day
+    @date = DailyDatum.last_common_day.day
   end
 
+
+  # def get_last_date
+  #   last_dates = Calendar.last(5).reverse
+  #   last_dates.each do |last_date|
+  #     number_of_fund_for_date = DailyDatum.where(calendar_id: last_date.id).count
+  #     puts number_of_fund_for_date
+  #     number_of_funds = Fund.count
+  #     puts number_of_funds
+  #     return date = last_date if number_of_fund_for_date == 17
+  #   end
+
+  # end
 end
--- app/models/daily_datum.rb
+++ app/models/daily_datum.rb
@@ -1,4 +1,15 @@
 class DailyDatum < ApplicationRecord
   belongs_to :fund
   belongs_to :calendar
+
+  def last_common_day
+    last_dates = Calendar.last(5).reverse
+    last_dates.each do |last_date|
+      number_of_fund_for_date = DailyDatum.where(calendar_id: last_date.id).count
+      puts number_of_fund_for_date
+      number_of_funds = Fund.count
+      puts number_of_funds
+      return date = last_date if number_of_fund_for_date == 17
+    end
+  end
 end
