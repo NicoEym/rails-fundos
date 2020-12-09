34e9e0ad0c0ba9e8b59bfd394550ec1f110ef370
nicolas <nicolas.eymenier@gmail.com>
2020-12-08 21:24:31 -0300

    ok data november fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -257,7 +257,6 @@ def write_monthly_data_fund(path_array, csv_options)
 
     fund_name = row['Nome']
     fund = Fund.find_by(name: fund_name)
-    puts fund.name
 
     date = get_date(row['Date'])
     puts date.day
